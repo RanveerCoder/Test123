18d5212b786c2045ef9b264cc283c17ac023934c
RanveerCoder <84279510+RanveerCoder@users.noreply.github.com>
2021-05-24 20:34:10 +0530

    test
    test

--- mail.php
+++ mail.php
@@ -4,11 +4,11 @@
 $name = $_POST['name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
-$to = "sdmpsnaigaon@mail.com";
+$to = "sdmpsnaigaon@gmail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
 $headers = "From: noreply@yoursite.com" . "\r\n" .
-"CC: factpathshalaoficial.com";
+"CC: factpathshalaoficial@gmail.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
